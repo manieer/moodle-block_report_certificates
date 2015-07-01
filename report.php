ecfeadbfaed8c201d2b980fd17453475c4e8d834
Manir Dhakal <manieer@gmail.com>
2015-07-01 23:25:46 +0100

    Revert 0024aa4..c5394b9
    This rolls back to commit 0024aa4c1eba59d4a5bd4614a8b16e44a4b2a028.

--- report.php
+++ report.php
@@ -74,14 +74,14 @@ $PAGE->set_url($url);
                              ON ctx.instanceid = cm.id
                      INNER JOIN {files} f
                              ON f.contextid = ctx.id
-                          WHERE cm.module = 4 AND
+                          WHERE cm.module = 23 AND
                                 ctx.contextlevel = 70 AND
                                 f.mimetype = 'application/pdf' AND
                                 ci.userid = f.userid AND
                                 ci.userid = :userid
                        GROUP BY ci.code
                        ORDER BY ci.timecreated ASC";
-        // CERTIFICATE MODULE (cm.module = 4), CONTEXT_MODULE (ctx.contextlevel = 70).
+        // CERTIFICATE MODULE (cm.module = 23), CONTEXT_MODULE (ctx.contextlevel = 70).
         // PDF FILES ONLY (f.mimetype = 'application/pdf').
         $certificates = $DB->get_records_sql($sql, array('userid' => $USER->id));
 
@@ -97,7 +97,7 @@ if (!$certificates) {
                 $certrecord->timecreated = $certdata->citimecreated;
 
                 // Date format.
-                $dateformat = get_string('strfdateshortmonth', 'langconfig');
+                $dateformat = get_string('strftimedate', 'langconfig');
 
                 // Required variables for output.
                 $userid = $certrecord->userid = $certdata->userid;
